160845f60a5562caf0b05c6ab7f7ed4a61e121ac
mp-gumi <basil725woodstock@gmail.com>
2021-10-28 18:15:52 +0900

    feat; initial commit

---
+++ src/components/Top/index.tsx
@@ -0,0 +1,4 @@
+function Top(): JSX.Element {
+  return <div>top</div>;
+}
+export default Top;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Top from "components/Top";
+import type { NextPage } from "next";
+
+const Pages: NextPage = () => {
+  return <Top />;
+};
+
+export default Pages;
